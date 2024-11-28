1dc5db39e53715e6ae7f8b1e3de284232aa0adbb
emmanuelniyoniringiye <160148727+emmanuelniyoniringiye@users.noreply.github.com>
2024-11-28 12:18:49 +0200

    First node.js task

--- checknum.js
+++ checknum.js
@@ -1,13 +1,13 @@
 let even = 1,
     odd = 1;
 while (even <= 10) {
-    console.log(even);
+    //console.log(even);
     if (even % 2 === 0) {
         console.log(even);
     }
 }
 while (odd <= 10) {
-    console.log(`${even}`);
+    //console.log(`${even}`);
     if (odd % 2 === 1) {
         console.log(odd);
     }
